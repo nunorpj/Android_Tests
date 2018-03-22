e12e29558a7b29b56870c8fe1c40bd88dfc3fb74
Nuno Rafael <nunorpj@gmail.com>
2018-03-22 19:20:11 +0000

    sound
    added soud and play and pause btn

--- SoundDemo/app/src/main/java/nunorpj/github/com/sounddemo/MainActivity.java
+++ SoundDemo/app/src/main/java/nunorpj/github/com/sounddemo/MainActivity.java
@@ -2,6 +2,9 @@ package nunorpj.github.com.sounddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.sound);
+
+
+        Button btnPlay = (Button) findViewById(R.id.btnplay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPlayer.start();
+            }
+        });
+
+
+        Button btnPause = (Button) findViewById(R.id.btnpause);
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPlayer.pause();
+            }
+        });
+
     }
 }
