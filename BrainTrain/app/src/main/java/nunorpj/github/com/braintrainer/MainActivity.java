6674eabc61f1a11daef1a246e3d1c3d6199881b1
Nuno Rafael <nunorpj@gmail.com>
2018-04-05 18:49:32 +0100

    Done

--- BrainTrain/app/src/main/java/nunorpj/github/com/braintrainer/MainActivity.java
+++ BrainTrain/app/src/main/java/nunorpj/github/com/braintrainer/MainActivity.java
@@ -1,6 +1,8 @@
 package nunorpj.github.com.braintrainer;
 
 import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,9 +19,13 @@ public class MainActivity extends AppCompatActivity {
     Button btn2;
     Button btn3;
     Button startbtn;
+    Button btnPlayAgain;
     TextView tvSum;
     TextView tvResult;
     TextView tvScore;
+    TextView tvTime;
+    ConstraintLayout gameLayout;
+
 
     Random rand;
     ArrayList<Integer> answers;
@@ -35,17 +41,20 @@ public class MainActivity extends AppCompatActivity {
         tvSum = (TextView) findViewById(R.id.tv_sum);
         tvResult = findViewById(R.id.tv_result);
         tvScore = findViewById(R.id.tv_score);
+        tvTime = findViewById(R.id.tv_timer);
         startbtn = (Button) findViewById(R.id.btn_start);
         btn0 = findViewById(R.id.btn_0);
         btn1 = findViewById(R.id.btn_1);
         btn2 = findViewById(R.id.btn_2);
         btn3 = findViewById(R.id.btn_3);
+        btnPlayAgain = findViewById(R.id.btn_playAgain);
+        btnPlayAgain.setVisibility(View.INVISIBLE);
 
+        gameLayout = findViewById(R.id.GameLayout);
 
         rand = new Random();
         answers = new ArrayList<Integer>();
 
-        generateQuestion();
 
 
     }
@@ -80,12 +89,14 @@ public class MainActivity extends AppCompatActivity {
         btn2.setText(answers.get(2).toString());
         btn3.setText(answers.get(3).toString());
 
-        tvResult.setText("");
     }
 
 
     public void start(View view){
         startbtn.setVisibility(View.INVISIBLE);
+        gameLayout.setVisibility(View.VISIBLE);
+        playAgain(null);
+
     }
 
     public void chooseAnswer(View view){
@@ -101,6 +112,36 @@ public class MainActivity extends AppCompatActivity {
         tvScore.setText(score + "/" +numberOfQuestions);
 
         generateQuestion();
+    }
+
+
+
+    public void playAgain (View view){
+
+        score=0;
+        numberOfQuestions=0;
+        tvScore.setText(score + "/" +numberOfQuestions);
+        tvTime.setText("30s");
+        tvResult.setText("");
+
+        new CountDownTimer(30100, 1000) {
+            @Override
+            public void onTick(long l) {
+                tvTime.setText(String.valueOf(l/1000) + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                tvTime.setText("0s");
+                tvResult.setTextColor(Color.BLACK);
+                tvResult.setText("Your score: " + score + "/" +numberOfQuestions);
+                btnPlayAgain.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        generateQuestion();
+        generateQuestion();
+        btnPlayAgain.setVisibility(View.INVISIBLE);
+
     }
 
 }
